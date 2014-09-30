d0d303861f487392f7363eadf20b05e9209a979d
skobe <rdeleon@orchestronix.com>
2014-09-30 20:29:29 +0800

    added block class

---
+++ src/arrows/Block.java
@@ -0,0 +1,11 @@
+package arrows;
+
+import java.awt.image.BufferedImage;
+
+import com.golden.gamedev.object.Sprite;
+
+public class Block extends Sprite {
+	public Block(BufferedImage image, double x, double y) {
+		super(image, x, y);
+	}
+}
