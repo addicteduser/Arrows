a70b34e869414c1e8136aa2f44c1d87957d5d79a
skobe <rdeleon@orchestronix.com>
2014-09-30 23:53:18 +0800

    Arrows Automated

--- src/arrows/GameFrame.java
+++ src/arrows/GameFrame.java
@@ -1,9 +1,11 @@
 package arrows;
 
 import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
 
 import com.golden.gamedev.GameEngine;
 import com.golden.gamedev.GameObject;
+import com.golden.gamedev.object.SpriteGroup;
 
 public class GameFrame extends GameObject {
 	
@@ -19,7 +21,12 @@ public class GameFrame extends GameObject {
 	private Block hit;
 	private Block buffer;
 	
-	private int[] arrows = new int [7];
+	private Block[] arrows = new Block[7];
+	private int[] arrowsID = new int [7];
+	
+	private SpriteGroup ARROWS;
+	
+	private int pressed = 0;
 
 	public GameFrame(GameEngine parent) {
 		super(parent);
@@ -29,7 +36,7 @@ public class GameFrame extends GameObject {
 	public void initResources() {
 		background = new Block(getImage(BG), 0, 0);
 		hit = new Block(getImage(HIT), 481, 352);
-		buffer = new Block(getImage(BUFFER), 20, 352);	
+		buffer = new Block(getImage(BUFFER), 20, 352);
 		
 		createSet();
 	}
@@ -39,13 +46,17 @@ public class GameFrame extends GameObject {
 		background.render(gd);
 		hit.render(gd);
 		buffer.render(gd);
+		ARROWS.render(gd);
 	}
 
 	@Override
 	public void update(long l) {
+		readInput();
+		
 		background.update(l);
 		hit.update(l);
 		buffer.update(l);
+		ARROWS.update(l);
 	}
 	
 	public void createSet() {
@@ -53,12 +64,43 @@ public class GameFrame extends GameObject {
 		int min = 1;
 		int range = max - min + 1;
 		int rand;
+		int x = 26;
 		
-		for (int i = 0; i < 9; i++) {
+		ARROWS = new SpriteGroup("Arrows");
+		
+		for (int i = 0; i < 7; i++) {
 			rand = (int) (Math.random() * range) + min;
-			arrows[i] = rand;
-			
+			arrowsID[i] = rand;
+			arrows[i] = new Block(getImage(assignArrows(rand)), x, 242);
+			x += 85;
+			ARROWS.add(arrows[i]);
 		}
+	}
+	
+	public String assignArrows(int x) {
+		String ARROW = "";
+		switch(x) {
+			case 1: ARROW = UP;
+				break;
+			case 2: ARROW = RIGHT;
+				break;
+			case 3: ARROW = DOWN;
+				break;
+			case 4: ARROW = LEFT;
+				break;
+		}
+		return ARROW;
+	}
+	
+	public void readInput() {
+		if (keyPressed(KeyEvent.VK_UP))
+			pressed = 1;
+		else if (keyPressed(KeyEvent.VK_RIGHT))
+			pressed = 2;
+		else if (keyPressed(KeyEvent.VK_DOWN))
+			pressed = 3;
+		else if (keyPressed(KeyEvent.VK_LEFT))
+			pressed = 4;
 	}
 
 }
